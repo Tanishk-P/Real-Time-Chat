59593ba08538f9fbdaf5d29acdd7eef72771ec3f
tanishkparadkar <tanishkparadkar@gmail.com>
2023-03-28 23:17:52 +0530

    Added new changes.

--- src/CommonComponents/Sidebar.jsx
+++ src/CommonComponents/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './Navbar';
 import Search from './Search';
 import Chats from './Chats';
+import Logo from './Logo';
 
 function Sidebar() {
   return (
@@ -10,7 +11,7 @@ function Sidebar() {
       <Search />
       <Chats />
       <div className='homeInfo'>
-        
+        <span>Powered by <Logo height={24} width={24} /> Live Chat </span>
       </div>
     </div>
   )
