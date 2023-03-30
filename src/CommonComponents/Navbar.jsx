d23f7e426d06f80e8f7142c8d83e9093828ed6c3
tanishkparadkar <tanishkparadkar@gmail.com>
2023-03-30 16:49:53 +0530

    added popconfirm for logout.

--- src/CommonComponents/Navbar.jsx
+++ src/CommonComponents/Navbar.jsx
@@ -4,9 +4,19 @@ import MaleUser from './UserAvatar';
 import { MdLogout } from 'react-icons/md';
 import { PageRoutes } from '../utls/PageRoutes';
 import { useNavigate } from 'react-router-dom';
+import { message, Popconfirm } from 'antd';
 
 function Navbar() {
   const navigate = useNavigate();
+
+  const confirm = () => {
+    message.success('Logged out successfully. Visit Again!');
+    navigate(PageRoutes.login);
+  };
+
+  const cancel = () => {
+    message.error('Great! Glad that you are with us.');
+  };
 
   return (
     <div className='navbar'>
@@ -15,7 +25,17 @@ function Navbar() {
        {/* <img src='' width={32} height={32} alt='p' /> */}
        <MaleUser />
        <span>You</span>
-       <button onClick={() => navigate(PageRoutes.login)}><MdLogout size={20} /></button>
+        <Popconfirm
+          title='Logout'
+          description='Logging out so soon?'
+          onConfirm={confirm}
+          onCancel={cancel}
+          okText='Yes'
+          cancelText='No'
+          placement="bottomRight"
+        >
+          <button><MdLogout size={20} /></button>   
+        </Popconfirm>
       </div>
     </div>
   )
