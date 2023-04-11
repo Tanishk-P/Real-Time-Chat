fbaf077879bdd73286982bbf5766d5c66268c2e7
tanishkparadkar <tanishkparadkar@gmail.com>
2023-04-11 18:23:40 +0530

    user online in chat screen

--- src/CommonComponents/Message.jsx
+++ src/CommonComponents/Message.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useRef } from "react";
 import { AuthContext } from "../Context/AuthContext";
 import { ChatContext } from "../Context/ChatContext";
 import ChatDate from "../CommonComponents/ChatDate";
+import { Badge } from "antd";
 
 function Message({message}) {
   const { currentUser } = useContext(AuthContext);
@@ -17,16 +18,18 @@ function Message({message}) {
     <>
       <div ref={ref} className={`message ${message.senderId === currentUser.uid && "owner"}`}>
         <div className="messageInfo">
-          <img
-            src={
-              message.senderId === currentUser.uid
-                ? currentUser.photoURL
-                : data.user.photoURL
-            }
-            height={36} width={36}
-            style={{ borderRadius: '20px', objectFit: 'cover', display: message.senderId === currentUser.uid ? 'none' : 'block'}}
-            alt=""
-          />
+          <Badge dot color="green" offset={[-4,32]} style={{ display: message.senderId === currentUser.uid ? 'none' : 'block'}}>
+            <img
+              src={
+                message.senderId === currentUser.uid
+                  ? currentUser.photoURL
+                  : data.user.photoURL
+              }
+              height={36} width={36}
+              style={{ borderRadius: '20px', objectFit: 'cover', display: message.senderId === currentUser.uid ? 'none' : 'block'}}
+              alt=""
+            />
+          </Badge>
         </div>
         <div className="messageContent">
           <span><p>{message.text}</p>
